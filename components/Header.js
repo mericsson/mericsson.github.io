27ac9ceb11812b978730a0abddc2ce94e227df23
Marcus Ericsson <mericsson@gmail.com>
2017-03-27 10:40:24 -0700

    header

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,11 @@ class Header extends React.Component {
       <header className="site-header">
         <div className="wrapper">
           <Link to={'/'}>
-            <span className="site-title">Marcus Ericsson</span>
+            <span className="site-title">mericsson</span>
           </Link>
           <nav className="site-nav">
             <Link to={'/about/'}>
-              <span className="page-link">About</span>
+              <span className="page-link">about</span>
             </Link>
           </nav>
         </div>

47b995d3f96007e476b12c8b6b08314d3d442793
Marcus Ericsson <36717+mericsson@users.noreply.github.com>
2019-03-07 16:57:09 -0800

    gatsby blog starter

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('/Users/mericsson/dev/mericsson.github.io/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/mericsson/dev/mericsson.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/mericsson/dev/mericsson.github.io/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
+    },{
+      plugin: require('/Users/mericsson/dev/mericsson.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
